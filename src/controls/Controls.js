5a3db5d6742d22bcfeaf6dff257a70c458002dc2
Lambda_School_Loaner_206 <constancebeebe@gmail.com>
2019-12-16 18:24:07 -0500

    controls completed

--- src/controls/Controls.js
+++ src/controls/Controls.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Controls from './Controls';
 
-const Controls = props => {
-  const { locked, closed, toggleLocked, toggleClosed } = props;
+// Gate
 
-  return (
-    <div className="controls panel">
-      <button disabled={!closed} onClick={toggleLocked} className="toggle-btn">
-        {locked ? 'Unlock Gate' : 'Lock Gate'}
-      </button>
-      <button disabled={locked} onClick={toggleClosed} className="toggle-btn">
-        {closed ? 'Open Gate' : 'Close Gate'}
-      </button>
-    </div>
-  );
-};
+test("cannot be opened if it is locked", () => {
 
-export default Controls;
+    const wrapper = rtl.render(<Controls locked={true} closed={true}/>);
+    expect(wrapper.getByTestId("knob")).not.toBeEnabled();
+
+});
+
+test("cannot be closed if it is locked", () => {
+
+    const wrapper = rtl.render(<Controls locked={true} closed={false}/>);
+    expect(wrapper.getByTestId("knob")).not.toBeEnabled();
+
+});
+
+
+// // Controls Component
+// //
+// // provide buttons to toggle the `closed` and `locked` states.
+// // buttons' text changes to reflect the state the door will be in if clicked
+// // the closed toggle button is disabled if the gate is locked
+// // the locked toggle button is disabled if the gate is open
--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,14 @@
 // Test away!
+
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import Controls from './Controls';
+
+
+test('open and unlocked, cant be open if its locked', () => {
+    const { getByTestId } = render (<Controls />);
+    const button = getByTestId(/toggle-btn/i);
+    fireEvent.click(button);
+    getByTestId()
+})
